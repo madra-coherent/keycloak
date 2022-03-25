285c2b1319d445a26bf66ebbf1609867f134b3a8
Pascal Davoust <pascal.davoust@eptica.com>
2022-03-25 10:10:02 +0100

    Added javadoc

--- model/infinispan/src/main/java/org/keycloak/models/cache/infinispan/SourceReleasingLazyLoader.java
+++ model/infinispan/src/main/java/org/keycloak/models/cache/infinispan/SourceReleasingLazyLoader.java
@@ -5,6 +5,12 @@ import java.util.function.Supplier;
 
 import org.keycloak.models.resource.Releasable;
 
+/**
+ * Specialized flavour of {@link DefaultLazyLoader} which releases loaded entities
+ *
+ * @param <S> the type of the {@link Releasable} source entity to load from
+ * @param <D> the type of data being supplied
+ */
 public class SourceReleasingLazyLoader<S extends Releasable, D> extends DefaultLazyLoader<S, D> {
 
     public SourceReleasingLazyLoader(Function<S, D> loader, Supplier<D> fallback) {
